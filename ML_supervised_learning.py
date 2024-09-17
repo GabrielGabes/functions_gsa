9656ca0d6fb9b6e22e370fba50bb7a82ebeec71a
GabrielGabes <gabriel_s_anjos@yahoo.com>
2024-09-16 22:44:39 -0300

    atuaalizando tudooo

--- ML_supervised_learning.py
+++ ML_supervised_learning.py
@@ -1,7 +1,7 @@
 # Função para avaliação de modelos exibindo metricas de avaliação
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, RocCurveDisplay
 
-def avaliar_modelo(y_verdadeiro, y_predito, conjunto_nome, plotar_grafico=False):
+def avaliar_modelo(y_verdadeiro, y_predito, conjunto_nome="", plotar_grafico=False):
     """
     Função para avaliar e imprimir o relatório de classificação e a matriz de confusão.
     """
@@ -10,14 +10,14 @@ def avaliar_modelo(y_verdadeiro, y_predito, conjunto_nome, plotar_grafico=False)
     #else:
     # Metricas
     print('*' * 70)
-    print("Relatório de Classificação para o Conjunto de", conjunto_nome,":\n")
-    print(classification_report(y_verdadeiro, y_predito))
+    # Matriz
+    print("Matriz de Confusão:", conjunto_nome,":\n")
+    print(confusion_matrix(y_verdadeiro, y_predito))
     
     print('*' * 50)
     
-    # Matriz
-    print("Matriz de Confusão para o Conjunto de", conjunto_nome,":\n")
-    print(confusion_matrix(y_verdadeiro, y_predito))
+    print("Relatório de Classificação:", conjunto_nome,":\n")
+    print(classification_report(y_verdadeiro, y_predito))
     print('*' * 70)
 
     if plotar_grafico == True:
