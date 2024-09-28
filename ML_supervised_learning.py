1144d9cc493826270ba60fd048038a7d1ee5bd14
GabrielGabes <gabriel_s_anjos@yahoo.com>
2024-09-28 19:53:41 -0300

    correcoes

--- ML_supervised_learning.py
+++ ML_supervised_learning.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, RocCurveDisplay
+from sklearn.metrics import confusion_matrix, classification_report, RocCurveDisplay
 
 # Função para avaliação de modelos exibindo metricas de avaliação
 def avaliar_modelo(y_verdadeiro, y_teste, conjunto_nome="", plotar_grafico=False):
@@ -14,20 +14,20 @@ def avaliar_modelo(y_verdadeiro, y_teste, conjunto_nome="", plotar_grafico=False
     print('*' * 70)
     # Matriz
     print("Matriz de Confusão:", conjunto_nome,":\n")
-    print(confusion_matrix(y_verdadeiro, y_predito))
+    print(confusion_matrix(y_verdadeiro, y_teste))
     
     print('*' * 50)
     
     print("Relatório de Classificação:", conjunto_nome,":\n")
-    print(classification_report(y_verdadeiro, y_predito))
+    print(classification_report(y_verdadeiro, y_teste))
     print('*' * 70)
 
     if plotar_grafico == True:
-        display(RocCurveDisplay.from_predictions(y_verdadeiro, y_predito, name = conjunto_nome))
+        display(RocCurveDisplay.from_predictions(y_verdadeiro, y_teste, name = conjunto_nome))
 ########################################################################################################################################################
 
 # Função que cria tabela com todas metricas de avaliação em cada ponto de threshold
-def aval_modelo_corte_tabela(x_teste, y_teste, classificador, beta = 1):
+def aval_modelo_corte_tabela(x_teste, y_teste, classificador, beta = 1, pontos_de_corte = np.arange(0, 1.1, 0.10)):
         
     previsoes_proba = classificador.predict_proba(x_teste)
     probs_positivas = previsoes_proba[:, 1]
@@ -35,22 +35,19 @@ def aval_modelo_corte_tabela(x_teste, y_teste, classificador, beta = 1):
     # Inicializando um DataFrame para armazenar as métricas
     fd = pd.DataFrame(columns=['threshold', 'tn', 'fp', 'fn', 'tp'])
 
-    # Pontos de corte de 0 a 1, com passos de 0.10
-    pontos_de_corte = np.arange(0, 1.1, 0.10)
-
     # Loop pelos pontos de corte
-    for i in pontos_de_corte:
-        threshold = i
+    for threshold in pontos_de_corte:
         previsoes_personalizadas = (probs_positivas >= threshold).astype(int)
 
         # Calculando os valores de tn, fp, fn, tp
         tn = len(np.where((previsoes_personalizadas == 0) & (y_teste == 0))[0])
         fp = len(np.where((previsoes_personalizadas == 1) & (y_teste == 0))[0])
+        
         fn = len(np.where((previsoes_personalizadas == 0) & (y_teste == 1))[0])
         tp = len(np.where((previsoes_personalizadas == 1) & (y_teste == 1))[0])
 
         # Adicionando os resultados ao DataFrame 'fd'
-        fd = pd.concat([fd, pd.DataFrame([[i, tn, fp, fn, tp]], columns=fd.columns)])
+        fd = pd.concat([fd, pd.DataFrame([[threshold, tn, fp, fn, tp]], columns=fd.columns)])
 
     # Substituir valores NaN por 0
     fd.fillna(0, inplace=True)
@@ -61,86 +58,64 @@ def aval_modelo_corte_tabela(x_teste, y_teste, classificador, beta = 1):
     metricas_de_aval = ['acuracia', 'precisao', 'sensibilidade', 'especificidade', 'f1',
                         'valor_pre_posi', 'valor_pre_neg', 'taxa_falsos_positivos', 'taxa_falsos_negativos',
                         'fdr', 'fo_r', 'indice_youden', 'coef_matthews', 'fb_score']
-    fd[metricas_de_aval] = np.nan
+    fd[metricas_de_aval] = 0
 
     # Loop para calcular as métricas em cada linha
-    for l in range(len(fd)):
-        # Calcular Acurácia
-        if (fd['tn'][l] + fd['fp'][l] + fd['fn'][l] + fd['tp'][l]) != 0:
-            fd['acuracia'][l] = (fd['tp'][l] + fd['tn'][l]) / (fd['tn'][l] + fd['fp'][l] + fd['fn'][l] + fd['tp'][l])
-        else:
-            fd['acuracia'][l] = 0
+    for i in range(len(fd)):
+        # Acurácia
+        if (fd['tn'][i] + fd['fp'][i] + fd['fn'][i] + fd['tp'][i]) != 0:
+            fd['acuracia'][i] = (fd['tp'][i] + fd['tn'][i]) / (fd['tn'][i] + fd['fp'][i] + fd['fn'][i] + fd['tp'][i])
 
-        # Calcular Precisão
-        if (fd['tp'][l] + fd['fp'][l]) != 0:
-            fd['precisao'][l] = fd['tp'][l] / (fd['tp'][l] + fd['fp'][l])
-        else:
-            fd['precisao'][l] = 0
+        # Precisão
+        if (fd['tp'][i] + fd['fp'][i]) != 0:
+            fd['precisao'][i] = fd['tp'][i] / (fd['tp'][i] + fd['fp'][i])
 
-        # Calcular Sensibilidade (Recall)
-        if (fd['tp'][l] + fd['fn'][l]) != 0:
-            fd['sensibilidade'][l] = fd['tp'][l] / (fd['tp'][l] + fd['fn'][l])
-        else:
-            fd['sensibilidade'][l] = 0
+        # Sensibilidade (Recall)
+        if (fd['tp'][i] + fd['fn'][i]) != 0:
+            fd['sensibilidade'][i] = fd['tp'][i] / (fd['tp'][i] + fd['fn'][i])
 
-        # Calcular Especificidade
-        if (fd['tn'][l] + fd['fp'][l]) != 0:
-            fd['especificidade'][l] = fd['tn'][l] / (fd['tn'][l] + fd['fp'][l])
-        else:
-            fd['especificidade'][l] = 0
+        # Especificidade
+        if (fd['tn'][i] + fd['fp'][i]) != 0:
+            fd['especificidade'][i] = fd['tn'][i] / (fd['tn'][i] + fd['fp'][i])
 
-        # Calcular F1-Score
-        if (fd['precisao'][l] + fd['sensibilidade'][l]) != 0:
-            fd['f1'][l] = 2 * (fd['precisao'][l] * fd['sensibilidade'][l]) / (fd['precisao'][l] + fd['sensibilidade'][l])
-        else:
-            fd['f1'][l] = 0
+        # F1-Score
+        if (fd['precisao'][i] + fd['sensibilidade'][i]) != 0:
+            fd['f1'][i] = 2 * (fd['precisao'][i] * fd['sensibilidade'][i]) / (fd['precisao'][i] + fd['sensibilidade'][i])
 
-        # Calcular Valor Preditivo Positivo
-        if (fd['tp'][l] + fd['fp'][l]) != 0:
-            fd['valor_pre_posi'][l] = fd['tp'][l] / (fd['tp'][l] + fd['fp'][l])
-        else:
-            fd['valor_pre_posi'][l] = 0
+        # Valor Preditivo Positivo
+        if (fd['tp'][i] + fd['fp'][i]) != 0:
+            fd['valor_pre_posi'][i] = fd['tp'][i] / (fd['tp'][i] + fd['fp'][i])
 
-        # Calcular Valor Preditivo Negativo
-        if (fd['tn'][l] + fd['fn'][l]) != 0:
-            fd['valor_pre_neg'][l] = fd['tn'][l] / (fd['tn'][l] + fd['fn'][l])
-        else:
-            fd['valor_pre_neg'][l] = 0
+        # Valor Preditivo Negativo
+        if (fd['tn'][i] + fd['fn'][i]) != 0:
+            fd['valor_pre_neg'][i] = fd['tn'][i] / (fd['tn'][i] + fd['fn'][i])
 
-        # Calcular Taxa de Falsos Positivos
-        fd['taxa_falsos_positivos'][l] = 1 - fd['especificidade'][l]
+        # Taxa de Falsos Positivos
+        fd['taxa_falsos_positivos'][i] = 1 - fd['especificidade'][i]
 
-        # Calcular Taxa de Falsos Negativos
-        fd['taxa_falsos_negativos'][l] = 1 - fd['sensibilidade'][l]
+        # Taxa de Falsos Negativos
+        fd['taxa_falsos_negativos'][i] = 1 - fd['sensibilidade'][i]
 
-        # Calcular False Discovery Rate (FDR)
-        if (fd['tp'][l] + fd['fp'][l]) != 0:
-            fd['fdr'][l] = fd['fp'][l] / (fd['tp'][l] + fd['fp'][l])
-        else:
-            fd['fdr'][l] = 0
+        # False Discovery Rate (FDR)
+        if (fd['tp'][i] + fd['fp'][i]) != 0:
+            fd['fdr'][i] = fd['fp'][i] / (fd['tp'][i] + fd['fp'][i])
 
-        # Calcular False Omission Rate (FOR)
-        if (fd['tn'][l] + fd['fn'][l]) != 0:
-            fd['fo_r'][l] = fd['fn'][l] / (fd['tn'][l] + fd['fn'][l])
-        else:
-            fd['fo_r'][l] = 0
+        # False Omission Rate (FOR)
+        if (fd['tn'][i] + fd['fn'][i]) != 0:
+            fd['fo_r'][i] = fd['fn'][i] / (fd['tn'][i] + fd['fn'][i])
 
-        # Calcular Índice de Youden
-        fd['indice_youden'][l] = fd['sensibilidade'][l] + fd['especificidade'][l] - 1
+        # Índice de Youden
+        fd['indice_youden'][i] = fd['sensibilidade'][i] + fd['especificidade'][i] - 1
 
-        # Calcular Coeficiente de Matthews (MCC)
-        denom_matthews = (fd['tp'][l]+fd['fp'][l])*(fd['tp'][l]+fd['fn'][l])*(fd['tn'][l]+fd['fp'][l])*(fd['tn'][l]+fd['fn'][l])
+        # Coeficiente de Matthews (MCC)
+        denom_matthews = (fd['tp'][i]+fd['fp'][i])*(fd['tp'][i]+fd['fn'][i])*(fd['tn'][i]+fd['fp'][i])*(fd['tn'][i]+fd['fn'][i])
         if denom_matthews != 0:
-            fd['coef_matthews'][l] = (fd['tp'][l]*fd['tn'][l] - fd['fp'][l]*fd['fn'][l]) / np.sqrt(denom_matthews)
-        else:
-            fd['coef_matthews'][l] = 0
+            fd['coef_matthews'][i] = (fd['tp'][i]*fd['tn'][i] - fd['fp'][i]*fd['fn'][i]) / np.sqrt(denom_matthews)
 
-        # Calcular F-beta Score
-        denom_fb_score = (beta**2 * fd['valor_pre_posi'][l]) + fd['sensibilidade'][l]
+        # F-beta Score
+        denom_fb_score = (beta**2 * fd['valor_pre_posi'][i]) + fd['sensibilidade'][i]
         if denom_fb_score != 0:
-            fd['fb_score'][l] = (1 + beta**2) * (fd['valor_pre_posi'][l] * fd['sensibilidade'][l]) / denom_fb_score
-        else:
-            fd['fb_score'][l] = 0
+            fd['fb_score'][i] = (1 + beta**2) * (fd['valor_pre_posi'][i] * fd['sensibilidade'][i]) / denom_fb_score
 
     fd[metricas_de_aval] = fd[metricas_de_aval].round(3)
     return fd
@@ -225,14 +200,10 @@ def adicionar_previsoes(x_teste, y_teste, **modelos):
 #)
 #df_previsoes.sample(5)
 
-
 ########################################################################################################################################################
-
-
-import numpy as np
 import itertools
 
-def gridsearch_mult_models_threshold(fd, y='y', beta = 1, linspace_thresholds = list(range(10, 100, 10))):
+def gridsearch_mult_models_threshold(fd, y='y', beta = 1, linspace_thresholds = np.arange(10, 100, 10)):
 
     y_teste = fd[y]
     colunas_proba_modelos = fd.drop(y, axis=1).columns
@@ -251,7 +222,7 @@ def gridsearch_mult_models_threshold(fd, y='y', beta = 1, linspace_thresholds = 
             soma_classificacoes += np.where(fd[proba_modelos] >= combinacoes[i][count], 1, 0)
             count =+ 1
 
-        maioria = int(count/2+1)
+        maioria = int((count/2) + 1)
         # classificacao_final
         previsoes_personalizadas = np.where(soma_classificacoes >= maioria, 1, 0)
         #######################################################################################
@@ -261,6 +232,7 @@ def gridsearch_mult_models_threshold(fd, y='y', beta = 1, linspace_thresholds = 
         # Calculando os valores de tn, fp, fn, tp
         tn = len(np.where((previsoes_personalizadas == 0) & (y_teste == 0))[0])
         fp = len(np.where((previsoes_personalizadas == 1) & (y_teste == 0))[0])
+        
         fn = len(np.where((previsoes_personalizadas == 0) & (y_teste == 1))[0])
         tp = len(np.where((previsoes_personalizadas == 1) & (y_teste == 1))[0])
 
@@ -273,87 +245,66 @@ def gridsearch_mult_models_threshold(fd, y='y', beta = 1, linspace_thresholds = 
     metricas_de_aval = ['acuracia', 'precisao', 'sensibilidade', 'especificidade', 'f1',
                         'valor_pre_posi', 'valor_pre_neg', 'taxa_falsos_positivos', 'taxa_falsos_negativos',
                         'fdr', 'fo_r', 'indice_youden', 'coef_matthews', 'fb_score']
-    tabela[metricas_de_aval] = np.nan
+    tabela[metricas_de_aval] = 0
 
     # Loop para calcular as métricas em cada linha
-    for l in range(len(tabela)):
-        # Calcular Acurácia
-        if (tabela['tn'][l] + tabela['fp'][l] + tabela['fn'][l] + tabela['tp'][l]) != 0:
-            tabela['acuracia'][l] = (tabela['tp'][l] + tabela['tn'][l]) / (tabela['tn'][l] + tabela['fp'][l] + tabela['fn'][l] + tabela['tp'][l])
-        else:
-            tabela['acuracia'][l] = 0
+    for i in range(len(tabela)):
+        # Acurácia
+        if (tabela['tn'][i] + tabela['fp'][i] + tabela['fn'][i] + tabela['tp'][i]) != 0:
+            tabela['acuracia'][i] = (tabela['tp'][i] + tabela['tn'][i]) / (tabela['tn'][i] + tabela['fp'][i] + tabela['fn'][i] + tabela['tp'][i])
 
-        # Calcular Precisão
-        if (tabela['tp'][l] + tabela['fp'][l]) != 0:
-            tabela['precisao'][l] = tabela['tp'][l] / (tabela['tp'][l] + tabela['fp'][l])
-        else:
-            tabela['precisao'][l] = 0
+        # Precisão
+        if (tabela['tp'][i] + tabela['fp'][i]) != 0:
+            tabela['precisao'][i] = tabela['tp'][i] / (tabela['tp'][i] + tabela['fp'][i])
 
-        # Calcular Sensibilidade (Recall)
-        if (tabela['tp'][l] + tabela['fn'][l]) != 0:
-            tabela['sensibilidade'][l] = tabela['tp'][l] / (tabela['tp'][l] + tabela['fn'][l])
-        else:
-            tabela['sensibilidade'][l] = 0
+        # Sensibilidade (Recall)
+        if (tabela['tp'][i] + tabela['fn'][i]) != 0:
+            tabela['sensibilidade'][i] = tabela['tp'][i] / (tabela['tp'][i] + tabela['fn'][i])
 
-        # Calcular Especificidade
-        if (tabela['tn'][l] + tabela['fp'][l]) != 0:
-            tabela['especificidade'][l] = tabela['tn'][l] / (tabela['tn'][l] + tabela['fp'][l])
-        else:
-            tabela['especificidade'][l] = 0
+        # Especificidade
+        if (tabela['tn'][i] + tabela['fp'][i]) != 0:
+            tabela['especificidade'][i] = tabela['tn'][i] / (tabela['tn'][i] + tabela['fp'][i])
 
-        # Calcular F1-Score
-        if (tabela['precisao'][l] + tabela['sensibilidade'][l]) != 0:
-            tabela['f1'][l] = 2 * (tabela['precisao'][l] * tabela['sensibilidade'][l]) / (tabela['precisao'][l] + tabela['sensibilidade'][l])
-        else:
-            tabela['f1'][l] = 0
+        # F1-Score
+        if (tabela['precisao'][i] + tabela['sensibilidade'][i]) != 0:
+            tabela['f1'][i] = 2 * (tabela['precisao'][i] * tabela['sensibilidade'][i]) / (tabela['precisao'][i] + tabela['sensibilidade'][i])
 
-        # Calcular Valor Preditivo Positivo
-        if (tabela['tp'][l] + tabela['fp'][l]) != 0:
-            tabela['valor_pre_posi'][l] = tabela['tp'][l] / (tabela['tp'][l] + tabela['fp'][l])
-        else:
-            tabela['valor_pre_posi'][l] = 0
+        # Valor Preditivo Positivo
+        if (tabela['tp'][i] + tabela['fp'][i]) != 0:
+            tabela['valor_pre_posi'][i] = tabela['tp'][i] / (tabela['tp'][i] + tabela['fp'][i])
 
-        # Calcular Valor Preditivo Negativo
-        if (tabela['tn'][l] + tabela['fn'][l]) != 0:
-            tabela['valor_pre_neg'][l] = tabela['tn'][l] / (tabela['tn'][l] + tabela['fn'][l])
-        else:
-            tabela['valor_pre_neg'][l] = 0
+        # Valor Preditivo Negativo
+        if (tabela['tn'][i] + tabela['fn'][i]) != 0:
+            tabela['valor_pre_neg'][i] = tabela['tn'][i] / (tabela['tn'][i] + tabela['fn'][i])
 
-        # Calcular Taxa de Falsos Positivos
-        tabela['taxa_falsos_positivos'][l] = 1 - tabela['especificidade'][l]
+        # Taxa de Falsos Positivos
+        tabela['taxa_falsos_positivos'][i] = 1 - tabela['especificidade'][i]
 
-        # Calcular Taxa de Falsos Negativos
-        tabela['taxa_falsos_negativos'][l] = 1 - tabela['sensibilidade'][l]
+        # Taxa de Falsos Negativos
+        tabela['taxa_falsos_negativos'][i] = 1 - tabela['sensibilidade'][i]
 
-        # Calcular False Discovery Rate (FDR)
-        if (tabela['tp'][l] + tabela['fp'][l]) != 0:
-            tabela['fdr'][l] = tabela['fp'][l] / (tabela['tp'][l] + tabela['fp'][l])
-        else:
-            tabela['fdr'][l] = 0
+        # False Discovery Rate (FDR)
+        if (tabela['tp'][i] + tabela['fp'][i]) != 0:
+            tabela['fdr'][i] = tabela['fp'][i] / (tabela['tp'][i] + tabela['fp'][i])
 
-        # Calcular False Omission Rate (FOR)
-        if (tabela['tn'][l] + tabela['fn'][l]) != 0:
-            tabela['fo_r'][l] = tabela['fn'][l] / (tabela['tn'][l] + tabela['fn'][l])
-        else:
-            tabela['fo_r'][l] = 0
+        # False Omission Rate (FOR)
+        if (tabela['tn'][i] + tabela['fn'][i]) != 0:
+            tabela['fo_r'][i] = tabela['fn'][i] / (tabela['tn'][i] + tabela['fn'][i])
 
-        # Calcular Índice de Youden
-        tabela['indice_youden'][l] = tabela['sensibilidade'][l] + tabela['especificidade'][l] - 1
+        # Índice de Youden
+        tabela['indice_youden'][i] = tabela['sensibilidade'][i] + tabela['especificidade'][i] - 1
 
-        # Calcular Coeficiente de Matthews (MCC)
-        denom_matthews = (tabela['tp'][l]+tabela['fp'][l])*(tabela['tp'][l]+tabela['fn'][l])*(tabela['tn'][l]+tabela['fp'][l])*(tabela['tn'][l]+tabela['fn'][l])
+        # Coeficiente de Matthews (MCC)
+        denom_matthews = (tabela['tp'][i]+tabela['fp'][i])*(tabela['tp'][i]+tabela['fn'][i])*(tabela['tn'][i]+tabela['fp'][i])*(tabela['tn'][i]+tabela['fn'][i])
         if denom_matthews != 0:
-            tabela['coef_matthews'][l] = (tabela['tp'][l]*tabela['tn'][l] - tabela['fp'][l]*tabela['fn'][l]) / np.sqrt(denom_matthews)
-        else:
-            tabela['coef_matthews'][l] = 0
+            tabela['coef_matthews'][i] = (tabela['tp'][i]*tabela['tn'][i] - tabela['fp'][i]*tabela['fn'][i]) / np.sqrt(denom_matthews)
 
-        # Calcular F-beta Score
-        denom_fb_score = (beta**2 * tabela['valor_pre_posi'][l]) + tabela['sensibilidade'][l]
+        # F-beta Score
+        denom_fb_score = (beta**2 * tabela['valor_pre_posi'][i]) + tabela['sensibilidade'][i]
         if denom_fb_score != 0:
-            tabela['fb_score'][l] = (1 + beta**2) * (tabela['valor_pre_posi'][l] * tabela['sensibilidade'][l]) / denom_fb_score
-        else:
-            tabela['fb_score'][l] = 0
-        ######################################################################################
+            tabela['fb_score'][i] = (1 + beta**2) * (tabela['valor_pre_posi'][i] * tabela['sensibilidade'][i]) / denom_fb_score
+    
+    ######################################################################################
     # Coluna final com os thresholds
     tabela = pd.concat([tabela, pd.DataFrame(combinacoes)], axis=1)
 
